e80ed809b887ccf14f77e497fbb0cd779ecbeafe
William <willspri@gmail.com>
2024-02-24 12:36:46 +1300

    Add files

---
+++ JavaTypes.java
@@ -0,0 +1,10 @@
+public class JavaTypes {
+	public static void main(String[] args) {
+		int age = 47;
+		double height = 5.9; //double is for storing floating point numbers with decimals
+		char gender = 'F';
+		String name = "Linda";
+		boolean isEmployee = true;
+		System.out.println(name + " is " + age + " years old and is gender " + gender);
+	}
+}
